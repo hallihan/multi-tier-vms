b32b85bc6e542e7452506e4c5bd28442881cbff1
Rick Hallihan <rickhall@microsoft.com>
2020-05-21 14:30:12 -0400

    Add screen dependency to simple web script

--- ExamplePostInstall2.sh
+++ ExamplePostInstall2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo ExamplePostInstall2.sh was run at $(date)
 if hash zypper; then
-  zypper install -y python3-pip
+  zypper install -y python3-pip screen
 else
-  yum install -y python3-pip
+  yum install -y python3-pip screen
 fi
 python3 -m pip install http.server
 echo "<html><body>Test HTTP Server Running on $(curl -H Metadata:true "http://169.254.169.254/metadata/instance/compute/name?api-version=2017-08-01&format=text")</body></html>" > index.html
