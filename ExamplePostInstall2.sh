d29af51fd36464d9c25aaba1e085f6eef7e24771
Rick Hallihan <rickhall@microsoft.com>
2020-05-21 13:56:45 -0400

    add global OS select param

--- ExamplePostInstall2.sh
+++ ExamplePostInstall2.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 echo ExamplePostInstall2.sh was run at $(date)
-zypper install python3-pip
+if hash zypper; then
+  zypper install -y python3-pip
+else
+  yum install -y python3-pip
+fi
 python3 -m pip install http.server
 echo "<html><body>Test HTTP Server Running on $(curl -H Metadata:true "http://169.254.169.254/metadata/instance/compute/name?api-version=2017-08-01&format=text")</body></html>" > index.html
 /usr/bin/screen -dmS simple-web python3 -m http.server 80
